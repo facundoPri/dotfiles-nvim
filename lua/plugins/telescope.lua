45486011c83406f2fa42c12826236cd2ecbd8cfa
Facundo Prieto <facundo.prieto321@gmail.com>
2026-05-19 02:07:59 -0300

    Add telescope picker for git status

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -100,6 +100,8 @@ return {
       vim.keymap.set('n', '<leader>sn', function()
         builtin.find_files { cwd = vim.fn.stdpath 'config' }
       end, { desc = '[S]earch [N]eovim files' })
+
+      vim.keymap.set('n', '<leader>sc', builtin.git_status, { desc = '[S]earch git [C]hanges' })
     end,
   },
 }
